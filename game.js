39ab276ab6ac601ffc778068b73c9605c97d6f7b
Harry-Merrell <Harrymerrell2012@googlemail.com>
2022-06-14 17:29:20 +0100

    Player Selection added + convert to lower case

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
+
+//Computer picks choice
 function computerPlay(option){
     let x = Math.floor(Math.random() * 3);
     
-
     if (x == 0){
         option = 'rock'
     } else if (x == 1){
@@ -9,7 +10,10 @@ function computerPlay(option){
     } else { 
         option = 'scissors'
     }
+
     return option;
     
 }
-console.log(computerPlay(2));
+//prompt player for choice
+let playerSelection = prompt("Rock, Paper, Scissors?");
+playerSelection = playerSelection.toLowerCase();
