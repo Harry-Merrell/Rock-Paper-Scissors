ece78e14e683d4e7fe676b4a35f7b1df130f3eb2
Harry-Merrell <Harrymerrell2012@googlemail.com>
2022-06-14 18:06:25 +0100

    Play round function added

--- game.js
+++ game.js
@@ -17,3 +17,65 @@ function computerPlay(option){
 //prompt player for choice
 let playerSelection = prompt("Rock, Paper, Scissors?");
 playerSelection = playerSelection.toLowerCase();
+console.log(playerSelection);
+let computerSelection;
+computerSelection = computerPlay();
+console.log(computerSelection)
+
+
+function playRound(playerSelection, computerSelection){
+let result;
+   switch(playerSelection){
+    case 'rock' : {
+        if(computerSelection == 'rock'){
+            result = ("Its a draw");
+            break;
+            
+        }else if (computerSelection == 'paper'){
+            result = ("You Lose!")
+            break;
+        }else{
+            result = console.log("You Win!");
+            break;
+
+        }
+    }
+    case 'paper' : {
+        if(computerSelection == 'paper'){
+            result = ("Its a draw");
+            break;
+            
+        }else if (computerSelection == 'scissors'){
+            result = ("You Lose!")
+            break;
+        }else{
+            result = console.log("You Win!");
+            break;
+
+        }
+       
+    }
+    case 'scissors' : {
+        if(computerSelection == 'scissors'){
+            result = ("Its a draw");
+            break;
+            
+        }else if (computerSelection == 'rock'){
+            result = ("You Lose!")
+            break;
+        }else{
+            result = console.log("You Win!");
+            break;
+
+        }
+   
+   }   
+   default:
+    result = "Please check spelling"
+   
+}
+return result;
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
