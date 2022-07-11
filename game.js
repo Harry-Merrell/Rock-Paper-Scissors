2c31a08d866c4e41e2ed12439e0cb2fe491d9d2b
Harry-Merrell <Harrymerrell2012@googlemail.com>
2022-07-11 09:46:14 +0100

    Rock paper scissors UI game created

--- game.js
+++ game.js
@@ -1,103 +1,163 @@
+//DOM 
+//container
+const container = document.querySelector('#container');
+//Displaying game result in div
+const gameResult = document.createElement('div');
 
+gameResult.textContent = "Select";
+gameResult.classList.add('default');
+container.appendChild(gameResult)
+
+//stats
+let winCount = 0;
+let gameCount = 0;
+
+
+
+const gameStatList = document.getElementById("gameStatList");
+var scoreTracker = document.createElement('li');
+scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+gameStatList.appendChild(scoreTracker);
+
+
+
+let playerSelection;
 //Computer picks choice
 function computerPlay(option){
     let x = Math.floor(Math.random() * 3);
-    
-    if (x == 0){
+    if (x === 0){
         option = 'rock'
-    } else if (x == 1){
+    } else if (x === 1){
         option = 'paper'
     } else { 
         option = 'scissors'
     }
 
     return option;
+}
+
+//on button press make selection and play round
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+    playerSelection = 'rock';
     
-}
-
-
-
-function playRound(playerSelection, computerSelection){
-let result;
-   switch(playerSelection){
-    case 'rock' : {
-        if(computerSelection == 'rock'){
-            result = "Its a draw";
-            break;
-            
-        }else if (computerSelection == 'paper'){
-            result = "You Lose!";
-            break;
-        }else{
-            result = "You Win!";
-            break;
-
-        }
-    }
-    case 'paper' : {
-        if(computerSelection == 'paper'){
-            result = "Its a draw";
-            break;
-            
-        }else if (computerSelection == 'scissors'){
-            result = "You Lose!";
-            break;
-        }else{
-            result = "You Win!";
-            break;
-
-        }
-       
-    }
-    case 'scissors' : {
-        if(computerSelection == 'scissors'){
-            result = "Its a draw";
-            break;
-            
-        }else if (computerSelection == 'rock'){
-            result = "You Lose!";
-            break;
-        }else{
-            result = "You Win!";
-            break;
-
-        }
-   
-   }   
-   default:
-    result = "Please check spelling";
-   
-}
-return result;
-}
-
-
-function game(){
-    let winCount = 0;
+    playRound();
     
+});
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+    playerSelection = 'paper';
+    playRound();
+    
+});
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    playRound();
+
+});
+
+function playRound(){
+    let computerSelection = computerPlay();
     let result;
-    for (let i = 0; i < 5; i++){
-        //play game
-        playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
-        
-        computerSelection = computerPlay();
 
-        console.log(playRound(playerSelection, computerSelection));
-        
+    switch(playerSelection){
+     case 'rock' : {
+         if(computerSelection == 'rock'){
+             result = "Its a draw.";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("draw")
+             break;
 
-        //Win Tracker
-        if(playRound(playerSelection, computerSelection) == "You Win!"){
+             
+         }else if (computerSelection == 'paper'){
+             result = "You Lose!";
+             gameResult.textContent = result;
+             gameResult.classList.remove("default", "win", "loss", "draw")
+             gameResult.classList.add("loss")
+             gameCount++
+             scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+             gameStatList.appendChild(scoreTracker);
+             break;
+         }else{
+             result = "You Win!";
+             gameResult.textContent = result;
+             gameResult.classList.remove("default", "win", "loss", "draw")
+             gameResult.classList.add("win")
+             winCount++
+             gameCount++
+             scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+             gameStatList.appendChild(scoreTracker);
+             break;
+ 
+         }
+     }
+     case 'paper' : {
+         if(computerSelection == 'paper'){
+            result = "Its a draw.";
+            console.log("Paper draw triggered")
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("draw")
+            break;
+             
+         }else if (computerSelection == 'scissors'){
+            result = "You Lose!";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("loss")
+            gameCount++
+            scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+            gameStatList.appendChild(scoreTracker);
+            break;
+         }else{
+            result = "You Win!";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("win")
             winCount++;
-        }
-}
-if(winCount >= 3){
-    return console.log("Congrats you won " + winCount + " rounds.");
-}else{
-    return console.log("You only won " + winCount + " round(s) out of 5 :(");
-}
-
-
-}
-
-
-game();
+            gameCount++;
+            scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+            gameStatList.appendChild(scoreTracker);
+            break;
+ 
+         }
+        
+     }
+     case 'scissors' : {
+         if(computerSelection == 'scissors'){
+            result = "Its a draw.";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("draw")
+            break;
+             
+         }else if (computerSelection == 'rock'){
+            result = "You Lose!";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw")
+            gameResult.classList.add("loss")
+            gameCount++;
+            scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+            gameStatList.appendChild(scoreTracker);
+            break;
+         }else{
+            result = "You Win!";
+            gameResult.textContent = result;
+            gameResult.classList.remove("default", "win", "loss", "draw");
+            gameResult.classList.add("win");
+            winCount++;
+            gameCount++;
+            scoreTracker.textContent = "Win Count: " + winCount + "/" + gameCount;
+            gameStatList.appendChild(scoreTracker);
+            break;
+         }
+    
+    }   
+    
+ }
+ 
+ return console.log(result);
+ }
+ 
